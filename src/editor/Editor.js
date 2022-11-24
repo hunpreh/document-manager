cc8c1a3ada64dba9f2ff7dec375280275a7c644e
hunpreh <humberto.gumc@gmail.com>
2022-11-24 13:28:33 -0600

    Editor Window Design

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -1,16 +1,64 @@
 import "./Editor.css";
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Editor from "ckeditor5-custom-build/build/ckeditor";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+import { Layout, Row, Col, Tooltip, Button, Input, Tag } from "antd";
+import { FileTextOutlined, SaveOutlined } from "@ant-design/icons";
 
-class EditorCK extends Component {
-  editor = null;
+const { Header } = Layout;
 
-  render() {
-    return (
+const EditorCK = (props) => {
+  const [editr, setEditor] = useState(null);
+  const [content, setContent] = useState("<p>Cargando...</p>");
+
+  const saveHandler = (props) => {
+    console.log("Guardar", editr.getData());
+  };
+
+  const button = <Button onClick={saveHandler}>Guardar</Button>;
+
+  return (
+    <div>
+      <Header className="editor_header">
+        <Row>
+          <Col flex="50px" align="center">
+            <Link to="/documentos">
+              <Tooltip
+                title="Regresar a Documentos"
+                placement="bottomLeft"
+                color="blue"
+              >
+                <Button
+                  type="text"
+                  icon={<FileTextOutlined className="editor_logo" />}
+                />
+              </Tooltip>
+            </Link>
+          </Col>
+          <Col flex={1}>
+            <Input placeholder="Titulo del Documento" />
+          </Col>
+          <Col flex="100px" align="center">
+            <Tag color="#108ee9">Nuevo</Tag>
+          </Col>
+          <Col flex="auto" align="right" pull={1}>
+            <Button
+              type="primary"
+              shape="round"
+              icon={<SaveOutlined />}
+              onClick={saveHandler}
+            >
+              Guardar
+            </Button>
+          </Col>
+        </Row>
+      </Header>
+
       <CKEditor
         onReady={(editor) => {
-          console.log("Editor is ready to use!", editor);
+          console.log("El editor ya esta listo!", editor);
+          setContent("<p>Listo</p>");
 
           editor.ui
             .getEditableElement()
@@ -19,19 +67,19 @@ class EditorCK extends Component {
               editor.ui.getEditableElement()
             );
 
-          this.editor = editor;
+          setEditor(editor);
         }}
         onError={(error, { willEditorRestart }) => {
           if (willEditorRestart) {
-            this.editor.ui.view.toolbar.element.remove();
+            editr.ui.view.toolbar.element.remove();
           }
         }}
-        onChange={(event, editor) => console.log({ event, editor })}
+        //onChange={(event, editor) => console.log(editor.getData())}
         editor={Editor}
-        data="<p>Hello from CKEditor 5's decoupled editor!</p>"
+        data={content}
       />
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default EditorCK;
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,57 +1,60 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Layout, Row, Col, Typography } from "antd";
 import "./layout.css";
 import proanLogo from "../assets/proan_logo.png";
+import { useLocation } from "react-router-dom";
 
-import MenuNav from "./MenuNav.js"
+import MenuNav from "./MenuNav.js";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { Title } = Typography;
 
 const AppLayout = (props) => {
   const [collapsed, setCollapsed] = useState(true);
+  const location = useLocation();
 
   const onCollapseHandler = (toggle) => {
     setCollapsed(toggle);
   };
 
+  if (location.pathname === "/ckeditor")
+    return <Fragment>{props.children}</Fragment>;
+
   return (
     <Layout className={"layout-site"}>
-        <Sider collapsible collapsed={collapsed} onCollapse={onCollapseHandler}>
-          <Row type="flex" justify="center">
-            <Col align="middle">
-              <img src={proanLogo} alt="logo Proan" className={"layout-logo"} />
-            </Col>
-          </Row>
-          <MenuNav />
-        </Sider>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapseHandler}>
+        <Row type="flex" justify="center">
+          <Col align="middle">
+            <img src={proanLogo} alt="logo Proan" className={"layout-logo"} />
+          </Col>
+        </Row>
+        <MenuNav />
+      </Sider>
 
-        <Layout className={"site_layout"}>
-          <Header className={"layout-header"}>
-            <Title
-              className={"layout-title"}
-              level={3}
-              ellipsis={true}
-              style={{ color: "white" }}
-            >
-              Editor de Documentos SGD
-            </Title>
-          </Header>
+      <Layout className={"site_layout"}>
+        <Header className={"layout-header"}>
+          <Title
+            className={"layout-title"}
+            level={3}
+            ellipsis={true}
+            style={{ color: "white" }}
+          >
+            Editor de Documentos SGD
+          </Title>
+        </Header>
 
-          <Content className={"layout-content"}>
-            <div className={"layout-card flex-overflow"}>
-              <div className={"flex-overflow"}>
-                {props.children}
-              </div>
-            </div>
-          </Content>
+        <Content className={"layout-content"}>
+          <div className={"layout-card flex-overflow"}>
+            <div className={"flex-overflow"}>{props.children}</div>
+          </div>
+        </Content>
 
-          <Footer className={"layout-footer"}>
-            PROAN ©{new Date().getFullYear()} Created by Humberto Gutierrez
-          </Footer>
-        </Layout>
+        <Footer className={"layout-footer"}>
+          PROAN ©{new Date().getFullYear()} Created by Humberto Gutierrez
+        </Footer>
       </Layout>
-  )
+    </Layout>
+  );
 };
 
 export default AppLayout;

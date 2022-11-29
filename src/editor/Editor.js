3730eb593cc25ee1d829775f4f2e11e80e0f87a3
hunpreh <humberto.gumc@gmail.com>
2022-11-29 13:48:58 -0600

    saveDocument api

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -1,19 +1,58 @@
 import "./Editor.css";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import Editor from "ckeditor5-custom-build/build/ckeditor";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import { Layout, Row, Col, Tooltip, Button, Input, Tag } from "antd";
 import { FileTextOutlined, SaveOutlined } from "@ant-design/icons";
+import { saveDocument } from "../firebase/api";
+//import useHttp from "../hooks/use-http";
 
 const { Header } = Layout;
 
 const EditorCK = (props) => {
   const [editr, setEditor] = useState(null);
   const [content, setContent] = useState("<p>Cargando...</p>");
+  const [version, setVersion] = useState("1.0");
+  const [date, setDate] = useState()
+  const [title, setTitle] = useState()
 
-  const saveHandler = (props) => {
-    console.log("Guardar", editr.getData());
+  //const { sendRequest, status } = useHttp(saveDocument);
+  const location = useLocation();
+  const pathSnippets = location.pathname.split("/").filter((i) => i);
+  const id = pathSnippets.slice(1, 2)[0];
+
+  // useEffect(() => {
+  //   if (status === "completed") {
+  //     console.log("SI SE GUARDO")
+  //   }
+  // }, [status, history]);
+
+  const saveHandler = () => {
+    const content = editr.getData();
+
+    const documentData = {
+      title,
+      date,
+      version,
+      content,
+    };
+
+    console.log("Guardar", documentData);
+    saveDocument(id, documentData);
+    //onAddQuote({ author: enteredAuthor, text: enteredText });
+  };
+
+  const getCurrentDate = () => {
+    const d = new Date();
+    const day = d.getDate();
+    const month = d.getMonth() + 1;
+    const year = d.getFullYear();
+    if (month < 10) {
+      setDate(`${day}-0${month}-${year}`);
+    } else {
+      setDate(`${day}-${month}-${year}`);
+    } // DD-MM-YYYY
   };
 
   return (
@@ -35,10 +74,10 @@ const EditorCK = (props) => {
             </Link>
           </Col>
           <Col flex={1}>
-            <Input placeholder="Titulo del Documento" />
+            <Input placeholder="Titulo del Documento" maxLength={32} onChange={(t) => setTitle(t.target.value)}/>
           </Col>
           <Col flex="100px" align="center">
-            <Tag color="#108ee9">Nuevo</Tag>
+            <Tag color="#108ee9">Version {version}</Tag>
           </Col>
           <Col flex="auto" align="right" pull={1}>
             <Button
@@ -57,6 +96,7 @@ const EditorCK = (props) => {
         onReady={(editor) => {
           console.log("El editor ya esta listo!", editor);
           setContent("<p>Listo</p>");
+          getCurrentDate();
 
           editor.ui
             .getEditableElement()

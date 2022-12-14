173d7c9115889ba5739543855a9875a55c044573
hunpreh <humberto.gumc@gmail.com>
2022-12-14 08:32:40 -0600

    Create icons file

---
+++ src/assets/icons.js
@@ -0,0 +1,169 @@
+import {
+  CaretRightFilled,
+  RightCircleFilled,
+  PlayCircleFilled,
+  RightSquareFilled,
+  PlusSquareFilled,
+  ClockCircleFilled,
+  PieChartFilled,
+  BoxPlotFilled,
+  FundFilled,
+  SlidersFilled,
+  AndroidFilled,
+  ApiFilled,
+  AppstoreFilled,
+  BankFilled,
+  BookFilled,
+  BugFilled,
+  BulbFilled,
+  CarFilled,
+  CarryOutFilled,
+  CloudFilled,
+  CompassFilled,
+  ContainerFilled,
+  CrownFilled,
+  CustomerServiceFilled,
+  DashboardFilled,
+  DollarCircleFilled,
+  DropboxCircleFilled,
+  ExperimentFilled,
+  FireFilled,
+  FolderFilled,
+  GiftFilled,
+  GoldFilled,
+  HeartFilled,
+  RocketFilled,
+  ShopFilled,
+  ShoppingFilled,
+  TagFilled,
+  SmileFilled,
+  ThunderboltFilled,
+} from "@ant-design/icons";
+
+const data = [
+  {
+    icon: <CaretRightFilled />,
+  },
+  {
+    icon: <RightCircleFilled />,
+  },
+  {
+    icon: <PlayCircleFilled />,
+  },
+  {
+    icon: <RightSquareFilled />,
+  },
+  {
+    icon: <PlusSquareFilled />,
+  },
+  {
+    icon: <ClockCircleFilled />,
+  },
+  {
+    icon: <PieChartFilled />,
+  },
+  {
+    icon: <BoxPlotFilled />,
+  },
+  {
+    icon: <FundFilled />,
+  },
+  {
+    icon: <SlidersFilled />,
+  },
+  {
+    icon: <AndroidFilled />,
+  },
+  {
+    icon: <ApiFilled />,
+  },
+  {
+    icon: <AppstoreFilled />,
+  },
+  {
+    icon: <BankFilled />,
+  },
+  {
+    icon: <BookFilled />,
+  },
+  {
+    icon: <BugFilled />,
+  },
+  {
+    icon: <BulbFilled />,
+  },
+  {
+    icon: <CarFilled />,
+  },
+  {
+    icon: <CarryOutFilled />,
+  },
+  {
+    icon: <CloudFilled />,
+  },
+  {
+    icon: <CompassFilled />,
+  },
+  {
+    icon: <ContainerFilled />,
+  },
+  {
+    icon: <CrownFilled />,
+  },
+  {
+    icon: <CustomerServiceFilled />,
+  },
+  {
+    icon: <DashboardFilled />,
+  },
+  {
+    icon: <DollarCircleFilled />,
+  },
+  {
+    icon: <DropboxCircleFilled />,
+  },
+  {
+    icon: <ExperimentFilled />,
+  },
+  {
+    icon: <FireFilled />,
+  },
+  {
+    icon: <FolderFilled />,
+  },
+  {
+    icon: <GiftFilled />,
+  },
+  {
+    icon: <GoldFilled />,
+  },
+  {
+    icon: <HeartFilled />,
+  },
+  {
+    icon: <RocketFilled />,
+  },
+  {
+    icon: <ShopFilled />,
+  },
+  {
+    icon: <ShoppingFilled />,
+  },
+  {
+    icon: <TagFilled />,
+  },
+  {
+    icon: <SmileFilled />,
+  },
+  {
+    icon: <ThunderboltFilled />,
+  },
+];
+
+export function getIcon(i) {
+  return data[i].icon;
+}
+
+export function getAllIcons() {
+  return data;
+}

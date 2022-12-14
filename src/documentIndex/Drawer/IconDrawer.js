6ed47c38b0073875e49de23cadacd1bbddacdf2c
hunpreh <humberto.gumc@gmail.com>
2022-12-14 09:38:58 -0600

    Create IconDrawer component

---
+++ src/documentIndex/Drawer/IconDrawer.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Drawer } from "antd";
+import { getIcon } from "../../assets/icons";
+
+import CustomIcon from "./CustomIcon";
+
+const IconDrawer = (props) => {
+  const [iColor, setIColor] = useState("#000000");
+  const [selectedIcon, setSelectedIcon] = useState();
+  const [iconIndex, setIconIndex] = useState(0);
+
+  useEffect(() => {
+    const icon = getIcon(iconIndex);
+    setSelectedIcon(icon);
+  }, [iconIndex]);
+
+  return (
+    <Drawer
+      title="Personaliza el ícono"
+      placement="right"
+      onClose={() => {
+        props.onClose();
+      }}
+      open={props.onOpen}
+    >
+      <CustomIcon
+        onChangeColor={(color) => {
+          setIColor(color);
+        }}
+        onSelectIcon={(i) => {
+          setIconIndex(i);
+        }}
+        iconColor={iColor}
+        iconSelected={selectedIcon}
+      />
+    </Drawer>
+  );
+};
+
+export default IconDrawer;

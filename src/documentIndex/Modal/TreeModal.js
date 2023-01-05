95b1cee2c711a769ac65256f8d3e4c8680c2e633
hunpreh <humberto.gumc@gmail.com>
2023-01-05 12:48:04 -0600

    Add components to TreeModal

--- src/documentIndex/Modal/TreeModal.js
+++ src/documentIndex/Modal/TreeModal.js
@@ -1,29 +1,57 @@
-import { Modal } from "antd";
-import React, { useState } from "react";
+import "./TreeModal.css"
+import { Modal, Tabs } from "antd";
+import React, { useState, useEffect } from "react";
+
+import GeneralOptions from "./GeneralOptions";
+import SecurityPermissions from "./SecurityPermissions";
+import ReferenceDocs from "./ReferenceDocs/ReferenceDocs";
 
 const TreeModal = (props) => {
-  const [confirmLoading, setConfirmLoading] = useState(false);
-  const [modalText, setModalText] = useState("Content of the modal");
+  const [title, setTitle] = useState();
+  const [isLeaf, setLeaf] = useState();
+  const [option, setOption] = useState();
+  const [category, setCategory] = useState();
 
-  const handleOk = () => {
-    setModalText("The modal will be closed after two seconds");
-    setConfirmLoading(true);
-    setTimeout(() => {
-      props.onCloseModal();
-      setConfirmLoading(false);
-    }, 2000);
-  };
+  useEffect(() => {
+    try {
+      setTitle(props.node.title);
+      setLeaf(props.node.isLeaf);
+      setCategory(props.node.category)
+      if (props.onOpen === "referencia") setOption(true);
+    } catch (error) {}
+  }, [props.onOpen]);
 
   return (
     <Modal
-      title="Title"
+      title={option ? `Documentos de referencia de "${title}"` : `Propiedades de "${title}"`}
       open={props.onOpen}
-      onOk={handleOk}
-      confirmLoading={confirmLoading}
-      onCancel={props.onCloseModal}
-      width={300}
+      width={620}
+      bodyStyle={{
+        paddingTop: 0,
+        maxHeight: "450px",
+        overflowY: "auto",
+        overflowX: "hidden",
+      }}
+      onCancel={() => {
+        props.onCloseModal(false);
+        setOption(false);
+      }}
+      destroyOnClose
+      footer={null}
     >
-      <p>{modalText}</p>
+      {isLeaf && option && <ReferenceDocs />}
+      {!option && (
+        <Tabs>
+          {!isLeaf && (
+            <Tabs.TabPane tab="Categoria" key="1">
+              <GeneralOptions category={category} />
+            </Tabs.TabPane>
+          )}
+          <Tabs.TabPane tab="Seguridad" key="2">
+            <SecurityPermissions />
+          </Tabs.TabPane>
+        </Tabs>
+      )}
     </Modal>
   );
 };
